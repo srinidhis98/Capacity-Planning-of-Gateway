9dda5c9c5f90cb0744cd6b435db5bad3f1f47e0f
Srinidhi Sivakumar <61039714+srinidhis98@users.noreply.github.com>
2020-05-02 03:23:43 -0700

    added filter-data fun

--- combine_data.py
+++ combine_data.py
@@ -21,22 +21,39 @@ def combine_data(file1, file2, file3):
         df_out.to_csv(file3, index=False)
 
 
-# def filter_data():
-#     file = input("Enter file name to filter")
-#     df = pd.read_csv(file)
-#     print(df.columns)
-#     print(df.dtypes)ye
+def filter_data(*args, file1):
+    print(args)
+    df1 = pd.read_csv(file1)
+    # print(df1.loc[df1['Time'] == args[0]])
+    # print(df1.loc[df1['Time'] == args[1]])
+    for items in args:
+        print(df1.loc[df1['Time'] == items])
 
 
 def main():
-    path = Path()
-    for file in path.glob('*.csv'):
-        print(file)
-    f1 = input("Enter file1 to combine")
-    f2 = input("Enter file2 to combine")
-    f3 = input("Enter the output file")
-    combine_data(file1=f1, file2=f2, file3=f3)
-    # filter_data()
+    while True:
+        choice = int(input("Enter a Choice\n1. Combine Data\n2. Filter data\n3. exit"))
+        if choice == 1:
+            path = Path()
+            for file in path.glob('*.csv'):
+                print(file)
+            f1 = input("Enter file1 to combine")
+            f2 = input("Enter file2 to combine")
+            f3 = input("Enter the output file")
+            combine_data(file1=f1, file2=f2, file3=f3)
+            continue
+
+        elif choice == 2:
+            path = Path()
+            for file in path.glob('*.csv'):
+                print(file)
+            f = input("Enter file to filter")
+            # timestamp = input('Enter the timestamp')
+            filter_data('3:50:40 AM', '3:50:41 AM', file1=f)
+            continue
+
+        else:
+            exit()
 
 
 if __name__ == '__main__':
