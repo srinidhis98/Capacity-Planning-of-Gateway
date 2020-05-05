5daad22f59207543645b6030e69d54f464681a4e
Srinidhi Sivakumar <61039714+srinidhis98@users.noreply.github.com>
2020-05-04 22:46:50 -0700

    Added Tabulate, changed filter_data

--- combine_data.py
+++ combine_data.py
@@ -1,5 +1,8 @@
+
 from pathlib import Path
+from tabulate import tabulate
 import sys
+import os
 import pandas as pd
 import plotly.express as px
 
@@ -33,25 +36,39 @@ def combine_data(file1, file2, file3):
             print('Value Error')
             drop_duplicate_choice = 'yes'
 
-
-    else:
+    if drop_duplicate_choice.lower() == 'no':
         data_frame_out.to_csv(file3, index=False)
+        print(f'{file3} created')
 
 
-def filter_data(*args, file1):
+def data_frame1(args):
+    pass
+
+
+def filter_data(data, file1, column_name):
     """
     This function filters the data and prints it based on the params specified
-    :param args:
+    :param data:
+    :param column_name:
     :param file1:
     :return:
     """
+    print(data)
+    d_frame = pd.read_csv(file1)
+    new_frame1 = tabulate(d_frame.loc[d_frame[column_name] == data], headers= d_frame.loc[
+        data == d_frame[column_name]].columns, tablefmt='psql', showindex=False)
+    # new_frame2 = d_frame.loc[d_frame[column_name] == data]
+    print(new_frame1)
+    # print(new_frame2)
+    save = input("Need to save as a file [Y/N]: ")
+    if save.lower() == 'y':
+        output = input('Enter file name with extension:')
+        file_open = open(output, 'w')
+        file_open.write(new_frame1)
+        # file_open.write(new_frame2)
 
-    print(args)
-    data_frame1 = pd.read_csv(file1)
-    # print(df1.loc[df1['Time'] == args[0]])
-    # print(df1.loc[df1['Time'] == args[1]])
-    for items in args:
-        print(data_frame1.loc[data_frame1['Time'] == items])
+    else:
+        pass
 
 
 def graphs(file, x_co, y_co):
@@ -68,40 +85,49 @@ def graphs(file, x_co, y_co):
 
 
 def main():
+    path = Path()
     while True:
         choice = int(input("Enter a Choice\n1. Combine Data\n2. Filter data\n3. Graphs\n4. exit"))
         if choice == 1:
-            path = Path()
+
             for file in path.glob('*.csv'):
                 print(file)
             file1 = input("Enter file1 to combine")
             file2 = input("Enter file2 to combine")
             file3 = input("Enter the output file")
-            if file1.count('.csv') == 1 and file2.count('.csv') == 1 and file3.count('.csv') == 1 \
-                    and file1 in path.glob('*.csv') \
-                    and file2 in path.glob('*.csv'):
+            # if '.csv' in file1:
+            #     print("Yes")
+            if '.csv' in file1 and '.csv' in file2 and '.csv' in file3 \
+                    and os.path.isfile(file1) \
+                    and os.path.isfile(file2):
                 combine_data(file1=file1, file2=file2, file3=file3)
             else:
                 print("Wrong File names")
 
         elif choice == 2:
-            path = Path()
+
             for file in path.glob('*.csv'):
                 print(file)
             file_name = input("Enter file to filter")
-            # timestamp = input('Enter the timestamp')
-            if file_name.count('.csv') == 1 and file_name in path.glob('*.csv'):
-                filter_data('3:50:40 AM', '3:50:41 AM', file1=file_name)
+            if '.csv' in file_name and os.path.isfile(file_name):
+                data_frame = pd.read_csv(file_name)
+                print(data_frame.columns)
+                col_name = input("Enter the column which is to be filtered")
+                data_to_be = input('Enter data to be filtered')
+                filter_data(data_to_be, file1=file_name, column_name=col_name)
             else:
                 print("Wrong File name")
 
         elif choice == 3:
-            path = Path()
+
             for file in path.glob('*.csv'):
                 print(file)
             file_name = input("Enter file for graph")
-            if file_name.count('.csv') == 1 and file_name in path.glob('*.csv'):
-                graphs(file_name, 'Time', 'Flow_count')
+            if '.csv' in file_name and os.path.isfile(file_name):
+                print(pd.read_csv(file_name).columns)
+                print("x_axis is taken as Time")
+                y_axis = input('Enter y_axis key')
+                graphs(file_name, 'Time', y_axis)
             else:
                 print("Wrong File name")
 
