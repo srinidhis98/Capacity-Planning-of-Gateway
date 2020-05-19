0b25f86ef808185c8ac76b766b226417f9a0b661
Srinidhi Sivakumar <61039714+srinidhis98@users.noreply.github.com>
2020-05-19 01:28:18 -0700

    running without errors-modification needed

--- sort.py
+++ sort.py
@@ -5,63 +5,83 @@ from tabulate import tabulate
 
 
 def print_data(min_df):
-    memory_used_percent = float(min_df['Used']) / float(min_df['Total']) * 100
+    new_df = min_df.iloc[0]
+    i = 0
+    while True:
+        if min_df['NAT'].iloc[i] == '-' or min_df['NAT'].iloc[i] == 'nan':
+            i += 1
+            continue
+        elif min_df['Flow_count'].iloc[i] == '-' or min_df['Flow_count'].iloc[i] == 'nan':
+            i += 1
+            continue
+        elif min_df['Handoffq_drops'].iloc[i] == '-' or min_df['Handoffq_drops'].iloc[i] == 'nan':
+            i += 1
+            continue
+        else:
+            new_df = min_df.iloc[i]
+            break
+
+    memory_used_percent = float(new_df['Used']) / float(new_df['Total']) * 100
     print("-----------------------------------")
-    print('Time: ', min_df['Time'])
-    print('CPU core:', min_df['CPU'])
-    print('Flow count:', int(min_df['Flow_count']))
-    print('Handoff drops:', int(min_df['Handoffq_drops']))
-    print('NAT:', min_df['NAT'])
-    print('CPU utlised:', float(100 - float(min_df['%idle'])))
+    print('Time: ', new_df['Time'])
+    print('CPU core:', new_df['CPU'])
+    print('Flow count:', int(new_df['Flow_count']))
+    print('Handoff drops:', int(new_df['Handoffq_drops']))
+    print('NAT:', new_df['NAT'])
+    print('CPU utlised:', float(100 - float(new_df['%idle'])))
     print('Memory Used:', memory_used_percent)
-    print('Network Bandwidth[kbps_in/ kbps_out]:', int(min_df['Kbps_in']), '/', int(min_df['Kbps_out']))
+    print('Network Bandwidth[kbps_in/ kbps_out]:', int(new_df['Kbps_in']), '/', int(new_df['Kbps_out']))
     print("-----------------------------------")
 
 
-def report(data_frame,name):
+def report(data_frame, name):
     sorted_data = remove_na(data_frame)
-    min_df = sorted_data.iloc[0]
-    print(min_df)
-    try:
+    # min_df = sorted_data.head(10)
+    print(tabulate(sorted_data.head(25), headers=sorted_data.columns, tablefmt='psql'))
+    # try:
+    print(f'{name}')
+    print_data(sorted_data)
+    print(f'CHECK IN \'report.txt\' FOR THIS DETAILS')
+    with open('report.txt', 'a') as file:
+        sys.stdout = file
         print(f'{name}')
-        print_data(min_df)
-        print(f'CHECK IN \'report.txt\' FOR THIS DETAILS')
-        with open('report.txt', 'a') as file:
-            sys.stdout = file
-            print(f'{name}')
-            print_data(min_df)
-            sys.stdout = sys.__stdout__
+        print_data(sorted_data)
+        sys.stdout = sys.__stdout__
 
-    except ValueError:
-        min_df = sorted_data.iloc[1]
-        print(min_df)
-        print(f'{name}')
-        print_data(min_df)
-        with open('report.txt', 'a') as file:
-            sys.stdout = file
-            print_data(min_df)
-            sys.stdout = sys.__stdout__
+    # except ValueError:
+    #     min_df = sorted_data.iloc[1]
+    #     print(min_df)
+    #     print(f'{name}')
+    #     print_data(min_df)
+    #     with open('report.txt', 'a') as file:
+    #         sys.stdout = file
+    #         print_data(min_df)
+    #         sys.stdout = sys.__stdout__
 
 
 def remove_na(data_frame):
     data_frame = data_frame.replace(r'^\s*$', np.nan, regex=True)
     # print(tabulate(df.head(10), headers=df.columns, showindex=False))
     data_frame = data_frame.sort_values('%idle', ignore_index=True, ascending=True, na_position='last')
-    data_frame.fillna(value='-', inplace=True)
-    print(tabulate(data_frame.head(10), headers=data_frame.columns, showindex=False, floatfmt='.1f'))
+    data_frame['%idle'] = data_frame['%idle'].astype('float')
+    data_frame['%idle'] = data_frame['%idle'].round(3)
+    data_frame1 = data_frame[data_frame['%idle'] >= 2.00]
+    print(tabulate(data_frame1.head(10), headers=data_frame.columns, showindex=False, floatfmt='.1f'))
+    data_frame1.fillna(value='-', inplace=True)
+    # print(tabulate(data_frame1.head(10), headers=data_frame.columns, showindex=False, floatfmt='.1f'))
 
-    return data_frame
+    return data_frame1
 
 
 def main():
     filename = input("enter a filename")
     df = pd.read_csv(filename)
-    options ={1:' Overall CPU utilisation', 2:'Core CPU utilisation'}
+    options = {1: ' Overall CPU utilisation', 2: 'Core CPU utilisation'}
     choice = input('1. Overall CPU utilisation\n2. Core CPU utilisation')
     if choice == '1':
         df = df.loc[df['CPU'] == ' all']
-        print(df.head(10))
-        df['NAT'] = df["NAT"].replace(float(0), 'Connection Timed out')
+        # print(df.head(10))
+        df['NAT'] = df["NAT"].replace(float(0), '-')
         # print(tabulate(df.head(15), headers=df.columns, tablefmt='psql',floatfmt='.1f'))
         if 'Unnamed: 0' in df.columns:
             df.drop(columns='Unnamed: 0', inplace=True)
@@ -69,7 +89,8 @@ def main():
         report(df, name=options[1])
 
     elif choice == '2':
-        df['NAT'] = df["NAT"].replace(float(0), 'Connection Timed out')
+        df = df.loc[df['CPU'] != ' all']
+        df['NAT'] = df["NAT"].replace(float(0), '-')
         # print(tabulate(df.head(15), headers=df.columns, tablefmt='psql',floatfmt='.1f'))
         if 'Unnamed: 0' in df.columns:
             df.drop(columns='Unnamed: 0', inplace=True)
